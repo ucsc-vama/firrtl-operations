712e5885ef88c62ecc6ca9d59f6770872185d46a
floofyjin <jinsungpark333@gmail.com>
2022-07-22 18:15:09 -0700

    change tests

--- model_uint.py
+++ model_uint.py
@@ -6,7 +6,7 @@ def getbitsize(value):
     return ceil(log2(value+1))
 
 class model_uint:
-    def __init__(self, bitsize=None, value=None):
+    def __init__(self, value, bitsize=None):
         if bitsize == None:
             realbitsize = getbitsize(value)
         else:
@@ -26,7 +26,7 @@ class model_uint:
         return False
 
     def uint_add(self, other):
-        return model_uint(max(self.bitsize,other.bitsize)+1, self.value+other.value)
+        return model_uint(self.value+other.value, max(self.bitsize,other.bitsize)+1)
 
     def uint_sub(self, other):
         comp = self.value - other.value
@@ -34,81 +34,81 @@ class model_uint:
         if (comp < 0):
             result = (abs(comp) ^ ((1<<size)-1))+1#2s complement
             result += (1<<size)
-            return model_uint(size+1, result)
+            return model_uint(result, size+1)
         else:
-            return model_uint(size+1, comp)
+            return model_uint(comp, size+1)
 
     def uint_mul(self, other):
-        return model_uint(self.bitsize+other.bitsize, self.value*other.value)
+        return model_uint(self.value*other.value, self.bitsize+other.bitsize)
 
     def uint_div(self, other): #division rouding down
         if other.value != 0:
-            return model_uint(self.bitsize, self.value//other.value)
+            return model_uint(self.value//other.value, self.bitsize)
 
     def uint_rem(self, other):
         if other.value != 0:
-            return model_uint(min(self.bitsize, other.bitsize), self.value % other.value)
+            return model_uint(self.value % other.value, min(self.bitsize, other.bitsize))
 
     def uint_lt(self, other):
         if self.value < other.value:
             return model_uint(1, 1)
-        return model_uint(1,0)
+        return model_uint(0,1)
 
     def uint_leq(self,other):
         if self.value <= other.value:
             return model_uint(1,1)
-        return model_uint(1,0)
+        return model_uint(0,1)
 
     def uint_gt(self,other):
         if self.value > other.value:
             return model_uint(1,1)
-        return model_uint(1,0)
+        return model_uint(0,1)
 
     def uint_geq(self,other):
         if self.value >= other.value:
             return model_uint(1,1)
-        return model_uint(1,0)
+        return model_uint(0,1)
 
     def uint_eq(self,other):
         if self.value == other.value:
             return model_uint(1,1)
-        return model_uint(1,0)
+        return model_uint(0,1)
     
     def uint_neq(self,other):
         if self.value != other.value:
             return model_uint(1,1)
-        return model_uint(1,0)
+        return model_uint(0,1)
 
     def uint_pad(self, n):
         if self.bitsize < n:
-            return model_uint(n, self.value)
+            return model_uint(self.value, n)
         else:
-            return model_uint(self.bitsize, self.value)
+            return model_uint(self.value, self.bitsize)
 
     def uint_asUint(self):
-        return model_uint(self.bitsize, self.value)
+        return model_uint(self.value, self.bitsize)
 
     def uint_asSint(self): #
-        return my_sint(self.bitsize, self.value)
+        return model_sint(self.value, self.bitsize)
     
     def uint_shl(self, n):
         num = self.value << n
-        return model_uint(self.bitsize+n, num)
+        return model_uint(num, self.bitsize+n)
 
     def uint_shr(self, n):
         num = self.value >> n
-        return model_uint(max(self.bitsize-n, 1), num)
+        return model_uint(num, max(self.bitsize-n, 1))
 
     def uint_dshl(self, other):
         num = self.value << other.value
-        return model_uint(self.bitsize + 2**other.bitsize -1, num)
+        return model_uint(num,self.bitsize + 2**other.bitsize -1)
 
     def uint_dshr(self, other): 
         num = self.value >> other.value
-        return model_uint(self.bitsize, num)
+        return model_uint(num, self.bitsize)
 
     def uint_cvt(self): #
-        return my_sint(self.bitsize+1, self.value)
+        return model_sint(self.value, self.bitsize+1)
 
     def uint_neg(self): #
         result = 0
@@ -116,61 +116,61 @@ class model_uint:
             result |= ((~self.value>>i)&1) << i
         result += 1
         result |= (1<<self.bitsize)
-        return my_sint(self.bitsize+1, result)
+        return model_sint(result, self.bitsize+1)
 
     def uint_not(self):
         result = 0
         for i in range(self.bitsize):
             result |= ((~self.value>>i)&1) << i
-        return model_uint(self.bitsize, result)
+        return model_uint(result, self.bitsize)
 
     def uint_and(self, other):
-        return model_uint(max(self.bitsize,other.bitsize), self.value & other.value)
+        return model_uint(self.value & other.value, max(self.bitsize,other.bitsize))
 
     def uint_or(self, other):
-        return model_uint(max(self.bitsize,other.bitsize), self.value | other.value)
+        return model_uint(self.value | other.value, max(self.bitsize,other.bitsize))
 
     def uint_xor(self, other):
-        return model_uint(max(self.bitsize,other.bitsize), self.value ^ other.value)
+        return model_uint(self.value ^ other.value, max(self.bitsize,other.bitsize))
     
     def uint_andr(self):
         result = 1
         for i in range(self.bitsize):
             bit_i = (self.value & (1<<i))>>i
             result = result & bit_i
-        return model_uint(1, result)
+        return model_uint(result, 1)
     
     def uint_orr(self):
         result = 0
         for i in range(self.bitsize):
             bit_i = (self.value & (1<<i))>>i
             result = result | bit_i
-        return model_uint(1, result)
+        return model_uint(result, 1)
 
     def uint_xorr(self):
         result = 0
         for i in range(self.bitsize):
             bit_i = (self.value & (1<<i))>>i
             result = result ^ bit_i
-        return model_uint(1, result)
+        return model_uint(result, 1)
 
     def uint_cat(self,other):
-        return model_uint(self.bitsize+other.bitsize, (self.value<<other.bitsize)|other.value)
+        return model_uint((self.value<<other.bitsize)|other.value, self.bitsize+other.bitsize)
 
     def uint_bits(self, hi, lo):
         if hi > lo and hi < self.bitsize:
-            return model_uint(hi-lo+1, (self.value>>lo)&(2**(hi-lo+1)-1))
-        return model_uint(1,0)
+            return model_uint((self.value>>lo)&(2**(hi-lo+1)-1), hi-lo+1)
+        return model_uint(0,1)
 
     def uint_head(self, n):
         if n > 0 and n <= self.bitsize:
-            return model_uint(n, self.value>>(self.bitsize-n))
-        return model_uint(1,0)
+            return model_uint(self.value>>(self.bitsize-n), n)
+        return model_uint(0,1)
 
     def uint_tail(self, n):
         if self.bitsize > n:
-            return model_uint(self.bitsize-n, self.value & (2**(self.bitsize-n)-1))
-        return model_uint(1,0)
+            return model_uint(self.value & (2**(self.bitsize-n)-1), self.bitsize-n)
+        return model_uint(0,n)
 
     def tohex(self):
         mask = (1<<self.bitsize)-1
--- test.py
+++ test.py
@@ -3,82 +3,82 @@ from model_uint import *
 import unittest
 
 # Variables
-test1 = model_uint(1, 0x0)
-test2 = model_uint(1, 0x1)
-test3 = model_uint(2, 0x2)
-test4 = model_uint(16, 0xcafe)
-test5 = model_uint(16, 0xbebe)
-test6 = model_uint(80, 0x234901234823ad3e9283)
-test7 = model_uint(80, 0x3da3e90d8f93ab12d239)
-test8 = model_uint(240, 0xada3e90d8f93bc7eab12ddf2331939a1340134f12349b10231aca924ade9)
-test9 = model_uint(240, 0xd23aa349017b3ae3cba396810bef3910f931065a390bce31891009cee714)
+test1 = model_uint(0x0, 1)
+test2 = model_uint(0x1, 1)
+test3 = model_uint(0x2, 2)
+test4 = model_uint(0xcafe, 16)
+test5 = model_uint(0xbebe, 16)
+test6 = model_uint(0x234901234823ad3e9283, 80)
+test7 = model_uint(0x3da3e90d8f93ab12d239, 80)
+test8 = model_uint(0xada3e90d8f93bc7eab12ddf2331939a1340134f12349b10231aca924ade9, 240)
+test9 = model_uint(0xd23aa349017b3ae3cba396810bef3910f931065a390bce31891009cee714, 240)
 
 # Run test on uint variables
 class TestOperations(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(test1.uint_add(test2), model_uint(2, 0x1))
-        self.assertEqual(test2.uint_add(test3), model_uint(3, 0x3))
-        self.assertEqual(test4.uint_add(test5), model_uint(17, 0x189bc))
-        self.assertEqual(test7.uint_add(test8), model_uint(241, 0xada3e90d8f93bc7eab12ddf2331939a1340134f160ed9a0fc14054378022))
+        self.assertEqual(test1.uint_add(test2), model_uint(0x1, 1))
+        self.assertEqual(test2.uint_add(test3), model_uint(0x3,3))
+        self.assertEqual(test4.uint_add(test5), model_uint(0x189bc,17))
+        self.assertEqual(test7.uint_add(test8), model_uint(0xada3e90d8f93bc7eab12ddf2331939a1340134f160ed9a0fc14054378022, 241))
 
     def test_sub(self):
-        self.assertEqual(test3.uint_sub(test2), model_uint(3, 0x1))
-        self.assertEqual(test5.uint_sub(test1), model_uint(17, 0xbebe))
-        self.assertEqual(test7.uint_sub(test6), model_uint(81, 0x1a5ae7ea476ffdd43fb6))
-        self.assertEqual(test9.uint_sub(test8), model_uint(241, 0x2496ba3b71e77e652090b88ed8d5ff6fc52fd16915c21d2f576360aa392b))
+        self.assertEqual(test3.uint_sub(test2), model_uint(0x1, 3))
+        self.assertEqual(test5.uint_sub(test1), model_uint(0xbebe, 17))
+        self.assertEqual(test7.uint_sub(test6), model_uint(0x1a5ae7ea476ffdd43fb6, 81))
+        self.assertEqual(test9.uint_sub(test8), model_uint(0x2496ba3b71e77e652090b88ed8d5ff6fc52fd16915c21d2f576360aa392b, 241))
 
     def test_mul(self):
-        self.assertEqual(test3.uint_mul(test2), model_uint(3, 0x2))
-        self.assertEqual(test5.uint_mul(test1), model_uint(17, 0x0))
-        self.assertEqual(test7.uint_mul(test6), model_uint(160, 0x87efce0722fbc9512aafe0d33af9c8bb554152b))
-        self.assertEqual(test9.uint_mul(test8), model_uint(480, 0x8e983b10cefc08fd4b85dbe9517422d805d6ebbf80d66d15f6b73f2029c82edabf8cb30436c2b4abfeaea119aa6856856e4a8346d4d6a34e7248d534))
+        self.assertEqual(test3.uint_mul(test2), model_uint(0x2, 3))
+        self.assertEqual(test5.uint_mul(test1), model_uint(0x0, 17))
+        self.assertEqual(test7.uint_mul(test6), model_uint(0x87efce0722fbc9512aafe0d33af9c8bb554152b, 160))
+        self.assertEqual(test9.uint_mul(test8), model_uint(0x8e983b10cefc08fd4b85dbe9517422d805d6ebbf80d66d15f6b73f2029c82edabf8cb30436c2b4abfeaea119aa6856856e4a8346d4d6a34e7248d534, 480))
 
     def test_div(self):
-        self.assertEqual(test1.uint_div(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_div(test3), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_div(test5), model_uint(16, 0x1))
-        self.assertEqual(test9.uint_div(test6), model_uint(240, 0x5f53f78026545a5ee52bb010bdc7fc10b6d49cf9a))
+        self.assertEqual(test1.uint_div(test2), model_uint(0x0, 1))
+        self.assertEqual(test2.uint_div(test3), model_uint(0x0, 1))
+        self.assertEqual(test4.uint_div(test5), model_uint(0x1, 16))
+        self.assertEqual(test9.uint_div(test6), model_uint(0x5f53f78026545a5ee52bb010bdc7fc10b6d49cf9a, 240))
 
     def test_rem(self):
-        self.assertEqual(test1.uint_rem(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_rem(test3), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_rem(test5), model_uint(16, 0xc40))
-        self.assertEqual(test9.uint_rem(test6), model_uint(80, 0x18b5f39130a9ab57d746))
+        self.assertEqual(test1.uint_rem(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_rem(test3), model_uint(0x1,1))
+        self.assertEqual(test4.uint_rem(test5), model_uint(0xc40,16))
+        self.assertEqual(test9.uint_rem(test6), model_uint(0x18b5f39130a9ab57d746, 80))
 
     def test_lt(self):
-        self.assertEqual(test1.uint_lt(test2), model_uint(1, 0x1))
-        self.assertEqual(test2.uint_lt(test2), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_lt(test5), model_uint(1, 0x0))
+        self.assertEqual(test1.uint_lt(test2), model_uint(0x1,1))
+        self.assertEqual(test2.uint_lt(test2), model_uint(0x0,1))
+        self.assertEqual(test4.uint_lt(test5), model_uint(0x0,1))
         
     def test_leq(self):
-        self.assertEqual(test1.uint_leq(test2), model_uint(1, 0x1))
-        self.assertEqual(test2.uint_leq(test2), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_leq(test5), model_uint(1, 0x0))
+        self.assertEqual(test1.uint_leq(test2), model_uint(0x1,1))
+        self.assertEqual(test2.uint_leq(test2), model_uint(0x1,1))
+        self.assertEqual(test4.uint_leq(test5), model_uint(0x0,1))
         
     def test_gt(self):
-        self.assertEqual(test1.uint_gt(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_gt(test2), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_gt(test5), model_uint(1, 0x1))
+        self.assertEqual(test1.uint_gt(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_gt(test2), model_uint(0x0,1))
+        self.assertEqual(test4.uint_gt(test5), model_uint(0x1,1))
         
     def test_geq(self):
-        self.assertEqual(test1.uint_geq(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_geq(test2), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_geq(test5), model_uint(1, 0x1))
+        self.assertEqual(test1.uint_geq(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_geq(test2), model_uint(0x1,1))
+        self.assertEqual(test4.uint_geq(test5), model_uint(0x1,1))
 
     def test_eq(self):
-        self.assertEqual(test1.uint_eq(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_eq(test2), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_eq(test5), model_uint(1, 0x0))
+        self.assertEqual(test1.uint_eq(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_eq(test2), model_uint(0x1,1))
+        self.assertEqual(test4.uint_eq(test5), model_uint(0x0,1))
 
     def test_neq(self):
-        self.assertEqual(test1.uint_neq(test2), model_uint(1, 0x1))
-        self.assertEqual(test2.uint_neq(test2), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_neq(test5), model_uint(1, 0x1))
+        self.assertEqual(test1.uint_neq(test2), model_uint(0x1,1))
+        self.assertEqual(test2.uint_neq(test2), model_uint(0x0,1))
+        self.assertEqual(test4.uint_neq(test5), model_uint(0x1,1))
 
     def test_pad(self):
-        self.assertEqual(test1.uint_pad(5), model_uint(5, 0x0))
-        self.assertEqual(test4.uint_pad(2), model_uint(16, 0xcafe))
+        self.assertEqual(test1.uint_pad(5), model_uint(0x0,5))
+        self.assertEqual(test4.uint_pad(2), model_uint(0xcafe,16))
 
     # #def test_asSint(self):
     #     #self.assertEqual(test1.uint_asSint(), my_sint(1, 0x0))
@@ -87,26 +87,26 @@ class TestOperations(unittest.TestCase):
     #     #self.assertEqual(test9.uint_asSint(), my_sint(80, 0x0))
 
     def test_shl(self):
-        self.assertEqual(test1.uint_shl(1), model_uint(2, 0x0))
-        self.assertEqual(test2.uint_shl(3), model_uint(4, 0x8))
-        self.assertEqual(test4.uint_shl(5), model_uint(21, 0x195fc0))
+        self.assertEqual(test1.uint_shl(1), model_uint(0x0,2))
+        self.assertEqual(test2.uint_shl(3), model_uint(0x8,4))
+        self.assertEqual(test4.uint_shl(5), model_uint(0x195fc0,21))
 
     def test_shr(self):
-        self.assertEqual(test2.uint_shr(1), model_uint(1, 0x0))
-        self.assertEqual(test3.uint_shr(3), model_uint(1, 0x0))
-        self.assertEqual(test5.uint_shr(5), model_uint(11, 0x5f5))
+        self.assertEqual(test2.uint_shr(1), model_uint(0x0,1))
+        self.assertEqual(test3.uint_shr(3), model_uint(0x0,1))
+        self.assertEqual(test5.uint_shr(5), model_uint(0x5f5,11))
 
     def test_dshl(self):
-        self.assertEqual(test2.uint_dshl(test1), model_uint(2, 0x1))
-        self.assertEqual(test3.uint_dshl(test2), model_uint(3, 0x4))
-        self.assertEqual(test4.uint_dshl(test3), model_uint(19, 0x32bf8))
-        self.assertEqual(test8.uint_dshl(test3), model_uint(243, 0x2b68fa4363e4ef1faac4b77c8cc64e684d004d3c48d26c408c6b2a492b7a4))
+        self.assertEqual(test2.uint_dshl(test1), model_uint(0x1,2))
+        self.assertEqual(test3.uint_dshl(test2), model_uint(0x4,3))
+        self.assertEqual(test4.uint_dshl(test3), model_uint(0x32bf8,19))
+        self.assertEqual(test8.uint_dshl(test3), model_uint(0x2b68fa4363e4ef1faac4b77c8cc64e684d004d3c48d26c408c6b2a492b7a4,243))
 
     def test_dshr(self):
-        self.assertEqual(test1.uint_dshr(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_dshr(test2), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_dshr(test3), model_uint(16, 0x32bf))
-        self.assertEqual(test9.uint_dshr(test3), model_uint(240, 0x348ea8d2405eceb8f2e8e5a042fbce443e4c41968e42f38c62440273b9c5))
+        self.assertEqual(test1.uint_dshr(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_dshr(test2), model_uint(0x0,1))
+        self.assertEqual(test4.uint_dshr(test3), model_uint(0x32bf,16))
+        self.assertEqual(test9.uint_dshr(test3), model_uint(0x348ea8d2405eceb8f2e8e5a042fbce443e4c41968e42f38c62440273b9c5,240))
 
     #def test_neg(self):
         #self.assertEqual(test1.uint_neg(), my_sint(1, 0x0))
@@ -115,66 +115,66 @@ class TestOperations(unittest.TestCase):
         #self.assertEqual(test9.uint_neg(), my_sint(240, 0x12dc55cb6fe84c51c345c697ef410c6ef06cef9a5c6f431ce76eff63118ec))
 
     def test_not(self):
-        self.assertEqual(test1.uint_not(), model_uint(1, 0x1))
-        self.assertEqual(test2.uint_not(), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_not(), model_uint(16, 0x3501))
-        self.assertEqual(test9.uint_not(), model_uint(240, 0x2dc55cb6fe84c51c345c697ef410c6ef06cef9a5c6f431ce76eff63118eb))
+        self.assertEqual(test1.uint_not(), model_uint(0x1, 1))
+        self.assertEqual(test2.uint_not(), model_uint(0x0,1))
+        self.assertEqual(test4.uint_not(), model_uint(0x3501,16))
+        self.assertEqual(test9.uint_not(), model_uint(0x2dc55cb6fe84c51c345c697ef410c6ef06cef9a5c6f431ce76eff63118eb, 240))
 
     def test_and(self):
-        self.assertEqual(test1.uint_and(test2), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_and(test3), model_uint(2, 0x0))
-        self.assertEqual(test4.uint_and(test5), model_uint(16, 0x8abe))
-        self.assertEqual(test9.uint_and(test6), model_uint(240, 0x210900210800090e8200))
+        self.assertEqual(test1.uint_and(test2), model_uint(0x0,1))
+        self.assertEqual(test2.uint_and(test3), model_uint(0x0,2))
+        self.assertEqual(test4.uint_and(test5), model_uint(0x8abe,16))
+        self.assertEqual(test9.uint_and(test6), model_uint(0x210900210800090e8200,240))
         
     def test_or(self):
-        self.assertEqual(test2.uint_or(test3), model_uint(2, 0x3))
-        self.assertEqual(test4.uint_or(test5), model_uint(16, 0xfefe))
-        self.assertEqual(test9.uint_or(test6), model_uint(240, 0xd23aa349017b3ae3cba396810bef3910f931065a3b4bcf33c933adfef797))
+        self.assertEqual(test2.uint_or(test3), model_uint(0x3,2))
+        self.assertEqual(test4.uint_or(test5), model_uint(0xfefe,16))
+        self.assertEqual(test9.uint_or(test6), model_uint(0xd23aa349017b3ae3cba396810bef3910f931065a3b4bcf33c933adfef797,240))
 
     def test_xor(self):
-        self.assertEqual(test1.uint_xor(test2), model_uint(1, 0x1))
-        self.assertEqual(test2.uint_xor(test3), model_uint(2, 0x3))
-        self.assertEqual(test4.uint_xor(test5), model_uint(16, 0x7440))
-        self.assertEqual(test9.uint_xor(test6), model_uint(240, 0xd23aa349017b3ae3cba396810bef3910f931065a1a42cf12c133a4f07597))
+        self.assertEqual(test1.uint_xor(test2), model_uint(0x1,1))
+        self.assertEqual(test2.uint_xor(test3), model_uint(0x3,2))
+        self.assertEqual(test4.uint_xor(test5), model_uint(0x7440,16))
+        self.assertEqual(test9.uint_xor(test6), model_uint(0xd23aa349017b3ae3cba396810bef3910f931065a1a42cf12c133a4f07597,240))
 
     def test_andr(self):
-        self.assertEqual(test1.uint_andr(), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_andr(), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_andr(), model_uint(1, 0x0))
-        self.assertEqual(test9.uint_andr(), model_uint(1, 0x0))
+        self.assertEqual(test1.uint_andr(), model_uint(0x0,1))
+        self.assertEqual(test2.uint_andr(), model_uint(0x1,1))
+        self.assertEqual(test4.uint_andr(), model_uint(0x0,1))
+        self.assertEqual(test9.uint_andr(), model_uint(0x0,1))
 
     def test_orr(self):
-        self.assertEqual(test1.uint_orr(), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_orr(), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_orr(), model_uint(1, 0x1))
-        self.assertEqual(test9.uint_orr(), model_uint(1, 0x1))
+        self.assertEqual(test1.uint_orr(), model_uint(0x0,1))
+        self.assertEqual(test2.uint_orr(), model_uint(0x1,1))
+        self.assertEqual(test4.uint_orr(), model_uint(0x1,1))
+        self.assertEqual(test9.uint_orr(), model_uint(0x1,1))
 
     def test_xorr(self):
-        self.assertEqual(test1.uint_xorr(), model_uint(1, 0x0))
-        self.assertEqual(test2.uint_xorr(), model_uint(1, 0x1))
-        self.assertEqual(test4.uint_xorr(), model_uint(1, 0x1))
-        self.assertEqual(test9.uint_xorr(), model_uint(1, 0x0))
+        self.assertEqual(test1.uint_xorr(), model_uint(0x0,1))
+        self.assertEqual(test2.uint_xorr(), model_uint(0x1,1))
+        self.assertEqual(test4.uint_xorr(), model_uint(0x1,1))
+        self.assertEqual(test9.uint_xorr(), model_uint(0x0,1))
         
     def test_cat(self):
-        self.assertEqual(test1.uint_cat(test2), model_uint(2, 0x1))
-        self.assertEqual(test2.uint_cat(test3), model_uint(3, 0x6))
-        self.assertEqual(test4.uint_cat(test5), model_uint(32, 0xcafebebe))
-        self.assertEqual(test9.uint_cat(test6), model_uint(320, 0x348ea8d2405eceb8f2e8e5a042fbce443e4c41968e42f38c62440273b9c5234901234823ad3e9283))
+        self.assertEqual(test1.uint_cat(test2), model_uint(0x1,2))
+        self.assertEqual(test2.uint_cat(test3), model_uint(0x6,3))
+        self.assertEqual(test4.uint_cat(test5), model_uint(0xcafebebe,32))
+        self.assertEqual(test9.uint_cat(test6), model_uint(0x348ea8d2405eceb8f2e8e5a042fbce443e4c41968e42f38c62440273b9c5234901234823ad3e9283,320))
 
     def test_bits(self):
-        self.assertEqual(test1.uint_bits(5, 1), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_bits(5, 1), model_uint(5, 0x1f))
-        self.assertEqual(test9.uint_bits(5, 1), model_uint(5, 0xa))
+        self.assertEqual(test1.uint_bits(5, 1), model_uint(0x0,1))
+        self.assertEqual(test4.uint_bits(5, 1), model_uint(0x1f,5))
+        self.assertEqual(test9.uint_bits(5, 1), model_uint(0xa,5))
 
     def test_head(self):
-        self.assertEqual(test1.uint_head(5), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_head(5), model_uint(5, 0x19))
-        self.assertEqual(test9.uint_head(5), model_uint(5, 0x1a))
+        self.assertEqual(test1.uint_head(5), model_uint(0x0,1))
+        self.assertEqual(test4.uint_head(5), model_uint(0x19,5))
+        self.assertEqual(test9.uint_head(5), model_uint(0x1a,5))
 
     def test_tail(self):
-        self.assertEqual(test1.uint_tail(10), model_uint(1, 0x0))
-        self.assertEqual(test4.uint_tail(10), model_uint(6, 0x3e))
-        self.assertEqual(test9.uint_tail(10), model_uint(230, 0x3aa349017b3ae3cba396810bef3910f931065a390bce31891009cee714))
+        self.assertEqual(test1.uint_tail(10), model_uint(0x0,1))
+        self.assertEqual(test4.uint_tail(10), model_uint(0x3e,6))
+        self.assertEqual(test9.uint_tail(10), model_uint(0x3aa349017b3ae3cba396810bef3910f931065a390bce31891009cee714,230))
 
 if __name__=="__main__":
     unittest.main()
