8c0374dc65bacd6a99386dae28883a5e57eab86e
floofyjin <jinsungpark333@gmail.com>
2022-10-23 22:59:18 -0700

    synt

--- model_sint.py
+++ model_sint.py
@@ -10,22 +10,6 @@ class model_sint:
             if self.value == other.value and self.bitsize == other.bitsize:
                 return True
         return False
-    
+
     def sint_add(self, other):
-        return model_sint(max(self.bitsize,other.bitsize)+1, self.value+other.value)
-
-    def sint_pad(self, n):
-        if self.bitsize < n:
-            return model_sint(n, self.value)
-        else:
-            return model_sint(self.bitsize, self.value)
-
-    def sint_cat(self, other):
-        return model_sint(self.bitsize + other.bitsize, (self.value << other.bitsize) | other.value)
-
-    def sint_asuint(self):
-        return model_uint(self.bitsize, self.value)
-
-    def print_bits(self):
-        result = "s(" + hex(self.value) + ")"
-        print(result, self.bitsize)
+        return model_uint(self.value+other.value, max(self.bitsize,other.bitsize)+1)
