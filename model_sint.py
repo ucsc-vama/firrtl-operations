0855c2ebf026e4de5bcb717c61720783f61566fe
floofyjin <jinsungpark333@gmail.com>
2022-11-15 00:02:29 -0800

    add working as intended probably again

--- model_sint.py
+++ model_sint.py
@@ -4,6 +4,12 @@ def two_comp(val, bits):
     val = val ^ ( (1 << bits) - 1)
     return (val +1)&((1<<bits)-1)
 
+def signed_subtract(a,b):
+    if b > a:
+        return two_comp(b-a)
+    return a - b
+
+
 class model_sint:
     def __init__(self, value, bitsize):
         self.bitsize = bitsize
@@ -15,23 +21,37 @@ class model_sint:
                 return True
         return False
 
+    # for a and b, if MSB is 1, then find 2's complement
+    # if MSB is 1, that means its negative, therefore do normal addition taking that into account
+    # if reslt is negative, return 2's complement
     def sint_add(self, other):
-        print(str(hex(two_comp(self.value, self.bitsize))))
-        print(str(hex(two_comp(other.value, other.bitsize))))
         asig = (self.value >> (self.bitsize - 1)) & 1
         bsig = (other.value >> (other.bitsize - 1)) & 1
-        maxlength = max(self.bitsize,other.bitsize)
-        minlength = min(self.bitsize,other.bitsize)
-        if asig ^ bsig: # different signs
-            val = (self.value + other.value) & ((1<<minlength)-1)
-        else: #same sign
-            val = self.value + other.value
-        return model_sint(val, maxlength+1)
+        maxbit=max(self.bitsize,other.bitsize)
+        a = self.value
+        b = other.value
+        if asig == 1:
+            a = two_comp(self.value, self.bitsize)
+        if bsig == 1:
+            b = two_comp(other.value, other.bitsize)
+        if asig > bsig:
+            val = signed_subtract(b,a)
+        elif asig < bsig:
+            val = signed_subtract(a,b)
+        elif asig and bsig:
+            val = a+b
+            val = two_comp(val, maxbit+1)
+        else:
+            val = a + b
+        return model_sint(val, maxbit+1)
 
     def sint_sub(self, other):
         val = self.value + two_comp(other.value, other.bitsize)
         return model_sint(val, max(self.bitsize,other.bitsize)+1)
 
+    # if result is negative, return 2's complement
+    # if val's MSB is 1, then find 2's complement. that is the real number
+    # do operation with the real number. if MSB was 1, find 2's complement
     def sint_mul(self, other):
         val = self.value * other.value
         return model_sint(val, self.bitsize + other.bitsize)
--- test_sint.py
+++ test_sint.py
@@ -15,15 +15,14 @@ test8 = model_sint(0xbeb828fdbac591dba8e38eeb433f563d, 128)
 class TestOperations(unittest.TestCase):
 
     def test_add(self):
-        # self.assertEqual(test1.sint_add(test2), model_sint(0x3a6c, 17))
-        # self.assertEqual(test2.sint_add(test2), model_sint(0x19964, 17))
-        # self.assertEqual(test3.sint_add(test4), model_sint(0x5003313223626d04, 65))
-        # self.assertEqual(test4.sint_add(test4), model_sint(0x1bdf5482bb20c4604, 65))
-        # self.assertEqual(test5.sint_add(test6), model_sint(0x27daaac790313f4c90d8, 81))
-        # self.assertEqual(test6.sint_add(test6), model_sint(0x1df7d15c1a7156fe6dbb4, 81))
-        # self.assertEqual(test7.sint_add(test8), model_sint(0x2cc16234c591abb694ea78ac80f45cb1, 129))
-        # self.assertEqual(test8.sint_add(test8), model_sint(0x17d7051fb758b23b751c71dd6867eac7a, 129))
-        test1.sint_add(test2).print_bits()
+        self.assertEqual(test1.sint_add(test2), model_sint(0x3a6c, 17))
+        self.assertEqual(test2.sint_add(test2), model_sint(0x19964, 17))
+        self.assertEqual(test3.sint_add(test4), model_sint(0x5003313223626d04, 65))
+        self.assertEqual(test4.sint_add(test4), model_sint(0x1bdf5482bb20c4604, 65))
+        self.assertEqual(test5.sint_add(test6), model_sint(0x27daaac790313f4c90d8, 81))
+        self.assertEqual(test6.sint_add(test6), model_sint(0x1df7d15c1a7156fe6dbb4, 81))
+        self.assertEqual(test7.sint_add(test8), model_sint(0x2cc16234c591abb694ea78ac80f45cb1, 129))
+        self.assertEqual(test8.sint_add(test8), model_sint(0x17d7051fb758b23b751c71dd6867eac7a, 129))
 
     def test_sub(self):
         self.assertEqual(test1.sint_sub(test2), model_sint(0xa108, 17))
@@ -35,13 +34,14 @@ class TestOperations(unittest.TestCase):
         # test2.sint_sub(test1).print_bits()
 
     def test_mul(self):
-        self.assertEqual(test1.sint_mul(test2), model_sint(0x5b8a, 17))
-        self.assertEqual(test3.sint_mul(test4), model_sint(0x5f7c3f3a3c3b3b04, 65))
-        self.assertEqual(test4.sint_mul(test4), model_sint(0x1c7e5a2a5d0a2a04, 65))
-        self.assertEqual(test5.sint_mul(test6), model_sint(0x2c6b1f6d8b6e1f4c90d8, 81))
-        self.assertEqual(test6.sint_mul(test6), model_sint(0x1e7e8a1c3d0e8f6e6dbb4, 81))
-        self.assertEqual(test7.sint_mul(test8), model_sint(0x2c7e5a2a5d0a2a04, 129))
-        self.assertEqual(test8.sint_mul(test8), model_sint(0x17e7e5a2a5d0a2a04, 129))
+        test1.sint_mul(test2).print_bits()
+        # self.assertEqual(test1.sint_mul(test2), model_sint(0xea028354, 32))
+    #     self.assertEqual(test3.sint_mul(test4), model_sint(0x5f7c3f3a3c3b3b04, 65))
+    #     self.assertEqual(test4.sint_mul(test4), model_sint(0x1c7e5a2a5d0a2a04, 65))
+    #     self.assertEqual(test5.sint_mul(test6), model_sint(0x2c6b1f6d8b6e1f4c90d8, 81))
+    #     self.assertEqual(test6.sint_mul(test6), model_sint(0x1e7e8a1c3d0e8f6e6dbb4, 81))
+    #     self.assertEqual(test7.sint_mul(test8), model_sint(0x2c7e5a2a5d0a2a04, 129))
+    #     self.assertEqual(test8.sint_mul(test8), model_sint(0x17e7e5a2a5d0a2a04, 129))
 
 if __name__=="__main__":
     unittest.main()
